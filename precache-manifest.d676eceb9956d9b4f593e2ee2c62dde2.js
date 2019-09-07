ab8ffbd9629c4171e8d8e5324ed95db466ec81bc
Quốc Hội <quocvu142@gmail.com>
2019-09-07 15:44:55 +0700

    Updates

---
+++ precache-manifest.d676eceb9956d9b4f593e2ee2c62dde2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e3994018365ea3d56e9d1915978c40",
+    "url": "/app-name/index.html"
+  },
+  {
+    "revision": "8c1c881b13e8dd7fe332",
+    "url": "/app-name/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6434ad6ed81aef9dc1fa",
+    "url": "/app-name/static/js/2.0aed37aa.chunk.js"
+  },
+  {
+    "revision": "8c1c881b13e8dd7fe332",
+    "url": "/app-name/static/js/main.688a3b6f.chunk.js"
+  },
+  {
+    "revision": "9bf8b33732033c3d7ff9",
+    "url": "/app-name/static/js/runtime~main.c28ede5f.js"
+  }
+]);
